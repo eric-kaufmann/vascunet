164c78a5e5a4b558e3e7f2abd5af7a35eeda8c68
Eric Kaufmann <eric.kaufmann@tu-ilmenau.de>
2024-05-05 10:16:44 +0200

    chore: Refactor vessel_dataloader module and add helper functions

--- src/vessel_dataloader.py
+++ src/vessel_dataloader.py
@@ -1,10 +1,12 @@
+from multiprocessing import freeze_support
 import torch
 from torch.utils.data import Dataset, DataLoader
 from utils.vessel_iterator import VesselIterator
 #from ..docs.config import DATA_DIR
 from pathlib import Path
+import utils.helper_functions as hf
+import numpy as np
 
-DATA_DIR = Path(r"C:\Users\Eric Kaufmann\workspace\MA\data\carotid_flow_database")
 
 class VesselDataset(Dataset):
     def __init__(self, data_path):
@@ -13,20 +15,29 @@ class VesselDataset(Dataset):
     def __len__(self):
         return len(self.vessel_iterator)
 
-    def __getitem__(self, idx):
-        sample = self.vessel_iterator[idx]
-        # Process the sample if needed
-        # ...
+    def __getitem__(self, idx, v_idx=0, n_rand=10000):
+        v = self.vessel_iterator[v_idx]
+        mesh_points = hf.move_and_rescale_matrix(v['mesh_points'])
+        data_points = hf.move_and_rescale_matrix(v['data_points'])
+        rand_idx = hf.pick_n_random_indices(mesh_points, n_rand)
+        
+        data = torch.tensor(
+            np.concatenate(([data_points[idx]], mesh_points[rand_idx])),
+            dtype=torch.float32
+        )
+        label = torch.tensor(
+            v['velocity_systolic'][idx], 
+            dtype=torch.float32
+        )
 
-        return sample
+        return data, label
 
-# Create an instance of the VesselDataset
-dataset = VesselDataset(DATA_DIR)
-
-# Set the batch size and other dataloader parameters
-batch_size = 32
-shuffle = True
-num_workers = 4
-
-# Create the dataloader
-dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
+if __name__ == "__main__":
+    DATA_DIR = Path(r"C:\Users\Eric Kaufmann\workspace\MA\data\carotid_flow_database")
+    freeze_support()
+    dataset = VesselDataset(DATA_DIR)
+    dataloader = DataLoader(dataset)
+    for i, batch in enumerate(dataloader):
+        print(f"Batch {i}: {batch}")
+        if i == 1:
+            break
