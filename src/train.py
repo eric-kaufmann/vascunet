6a0bd28b79cbd7a51933c988eeb24c0fb7db20fd
Eric Kaufmann <eric.kaufmann291@gmail.com>
2024-05-24 19:20:26 +0200

    create new dataloader for point patches

--- src/train.py
+++ src/train.py
@@ -1,20 +1,106 @@
 import lightning as L
-from pathlib import Path
-from utils.helper_functions import get_project_root
-from vessel_dataloader import VesselDataset
-from model import VesselModel
-from torch.utils.data import Dataset, DataLoader
+import torch
+from torch.utils.data import DataLoader
+from torch import optim, nn
+from lightning.pytorch.loggers import TensorBoardLogger
+from lightning.pytorch.callbacks import TQDMProgressBar
 
-DATA_DIR = get_project_root() / "data" / "carotid_flow_database"
+
+import utils.helper_functions as hf
+from vessel_dataloader import VesselDataset
+from models import NSModel, VesselGeomEmbedding
+
+
+DATA_DIR = hf.get_project_root() / "data" / "carotid_flow_database"
 BATCH_SIZE = 8
 
+class VesselModel(L.LightningModule):
+    def __init__(self, ns_model, vessel_geom_embedding_model, num_neighbours=32, num_midpoints=32):
+        super().__init__()
+        self.ns_model = ns_model
+        self.vessel_geom_embedding_model = vessel_geom_embedding_model
+        self.num_neighbours = num_neighbours
+        self.num_midpoints = num_midpoints
+
+    def training_step(self, batch, batch_idx):
+        mesh = batch['mesh_tensor']
+        fluid_points = batch['fluid_points']
+        sys_vel = batch['sys_vel_tensor']
+        
+        # get num_midpoints points index from farthest point sample which create center of patches
+        fps_index = hf.farthest_point_sample(mesh, self.num_midpoints)
+        # index to real points 
+        mid_points = hf.index_points(mesh, fps_index)
+        # calculate distances between the mesh_points and the new_points
+        dists = hf.square_distance(mid_points, mesh)
+        # get the indices of the num_neighbours closest points
+        dist_idx = dists.argsort()[:, :, :self.num_neighbours]
+        # group the points by the indices from dist_idx
+        grouped_points = hf.index_points(mesh, dist_idx)
+        # flatten the grouped points
+        grouped_points_flat = torch.flatten(grouped_points, start_dim=1)
+        
+        mesh_emb = self.vessel_geom_embedding_model(grouped_points_flat)
+        
+        mesh_sample_input = torch.concat((fluid_points, mesh_emb), dim=1)
+        
+        ns_pred = self.ns_model(mesh_sample_input)
+
+        loss = nn.functional.mse_loss(ns_pred, sys_vel)
+
+        self.log("train_loss", loss, prog_bar=False, on_step=True, on_epoch=True, batch_size=BATCH_SIZE)
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
+
 def main():
+    torch.set_float32_matmul_precision('medium')
+    
+    num_random_mesh_iterations=1
+    num_fluid_samples=10000
+    num_meshpoints=8192//2
+    seed=666
+    num_midpoints=32
+    num_neighbours=32
 
-    dataset = VesselDataset(DATA_DIR)
-    dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=2, persistent_workers=True)
-    model = VesselModel()
-
-    trainer = L.Trainer(max_epochs=5, limit_train_batches=200)
+    dataset = VesselDataset(
+        DATA_DIR, 
+        num_random_mesh_iterations=num_random_mesh_iterations, 
+        num_fluid_samples=num_fluid_samples, 
+        num_meshpoints=num_meshpoints, 
+        seed=seed
+    )
+    
+    dataloader = DataLoader(
+        dataset, 
+        batch_size=BATCH_SIZE, 
+        shuffle=False, 
+        num_workers=2, 
+        persistent_workers=True
+    )
+    
+    vessel_geom_embedding_model = VesselGeomEmbedding(
+        in_features=num_midpoints*num_neighbours*3, 
+        out_features=256
+    )
+    
+    ns_model = NSModel(
+        in_features=256+3, 
+        out_features=3
+    )
+    
+    model = VesselModel(ns_model, vessel_geom_embedding_model)
+    
+    logger = TensorBoardLogger("./lightning_logs", name="train")
+    
+    trainer = L.Trainer(
+        max_epochs=10, 
+        logger=logger, 
+        log_every_n_steps=2000,
+        callbacks=[TQDMProgressBar(refresh_rate=2000)]
+    )
     trainer.fit(model=model, train_dataloaders=dataloader)
 
 
--- src/vessel_dataloader.py
+++ src/vessel_dataloader.py
@@ -1,35 +1,134 @@
-from multiprocessing import freeze_support
 import torch
-from torch.utils.data import Dataset, DataLoader
-from utils.vessel_iterator import VesselIterator
-#from ..docs.config import DATA_DIR
-from pathlib import Path
-import utils.helper_functions as hf
-import numpy as np
+from torch.utils.data import Dataset
+import os
+import meshio
 
 
 class VesselDataset(Dataset):
-    def __init__(self, data_path):
-        self.vessel_iterator = VesselIterator(data_path)
-        self.v_index = 0
+    """
+    A PyTorch dataset for loading vessel data.
+
+    Parameters:
+    - data_path (str): The path to the directory containing the vessel data.
+    - samples_per_vessel (int): The number of samples to generate per vessel.
+    - num_meshpoints (int): The number of mesh points to sample per sample.
+    - seed (int): The random seed for reproducibility.
+
+    Attributes:
+    - data_path (str): The path to the directory containing the vessel data.
+    - samples_per_vessel (int): The number of samples to generate per vessel.
+    - vessel_files (list): A list of dictionaries, where each dictionary contains the paths to the fluid and mesh files.
+    - num_meshpoints (int): The number of mesh points to sample per sample.
+    - current_vessel (int): The index of the current vessel being processed.
+    - current_vessel_tensor (torch.Tensor): The tensor representing the current vessel's mesh data.
+
+    Methods:
+    - __len__(): Returns the total number of samples in the dataset.
+    - __getitem__(idx): Returns the sample at the given index.
+    - _update_vessel_tensor(vessel_idx): Updates the current vessel tensor based on the given vessel index.
+    - _get_vessel_files(): Retrieves the vessel files from the given directory.
+    """
+
+    def __init__(self, data_path, num_random_mesh_iterations=10_000, num_fluid_samples=50_000, num_meshpoints=8192, seed=None):
+        if seed is not None:
+            torch.manual_seed(seed)
+        else:
+            torch.seed()
+        self.data_path = data_path
+        self.samples_per_vessel = num_random_mesh_iterations
+        self.vessel_files = self._get_vessel_files()
+        self.num_meshpoints = num_meshpoints
+        self.num_fluid_samples = num_fluid_samples
+    
+        self.current_vessel = 0
+        self.current_vessel_mesh_tensor = self._update_vessel_mesh_tensor(0)
+        self.current_vessel_fluid_point_tensor = self._update_vessel_fluid_point_tensor(0)
+        self.current_vessel_sys_vel_tensor = self._update_vessel_sys_vel_tensor(0)
+    
 
     def __len__(self):
-        return len(self.vessel_iterator[self.v_index]['data_points'])
+        return len(self.vessel_files) * self.samples_per_vessel * self.num_fluid_samples
 
-    def __getitem__(self, idx, v_idx=0, n_rand=10000):
-        v = self.vessel_iterator[self.v_index]
+    def __getitem__(self, idx):
+        fluid_idx = idx % self.num_fluid_samples
+        temp_idx = idx // self.num_fluid_samples
+        vessel_idx = temp_idx // self.samples_per_vessel
+        mesh_iter_idx = temp_idx % self.samples_per_vessel
+        if vessel_idx != self.current_vessel:
+            self.current_vessel = vessel_idx
+            self.current_vessel_mesh_tensor = self._update_vessel_mesh_tensor(vessel_idx)
+            self.current_vessel_sys_vel_tensor = self._update_vessel_sys_vel_tensor(vessel_idx)
+            self.current_vessel_fluid_point_tensor = self._update_vessel_fluid_point_tensor(vessel_idx)
+         
+        return_dict = {
+            'mesh_tensor': self.current_vessel_mesh_tensor[mesh_iter_idx],
+            'fluid_points': self.current_vessel_fluid_point_tensor[fluid_idx],
+            'sys_vel_tensor': self.current_vessel_sys_vel_tensor[fluid_idx],
+        }    
         
-        mesh_points = hf.move_and_rescale_matrix(v['mesh_points'])
-        data_points = hf.move_and_rescale_matrix(v['data_points'])
-        rand_idx = hf.pick_n_random_indices(mesh_points, n_rand)
-        
-        data = torch.tensor(
-            np.concatenate(([data_points[idx]], mesh_points[rand_idx])),
-            dtype=torch.float32
-        )
-        label = torch.tensor(
-            v['velocity_systolic'][idx], 
-            dtype=torch.float32
-        )
+        return return_dict#self.current_vessel_mesh_tensor[sample_idx], self.current_vessel_sys_vel_tensor
+    
+    def _update_vessel_mesh_tensor(self, vessel_idx):
+        """
+        Updates the current vessel tensor based on the given vessel index.
 
-        return data.T, label
+        Parameters:
+        - vessel_idx (int): The index of the vessel.
+
+        Returns:
+        - torch.Tensor: The tensor representing the vessel's mesh data.
+        """
+        vessel = self.vessel_files[vessel_idx]
+        mesh_data = meshio.read(vessel['mesh'])
+        mesh_tensor = torch.Tensor(mesh_data.points)
+        random_sample_idx = torch.randint(0, mesh_tensor.shape[0], (self.samples_per_vessel,self.num_meshpoints))
+        return mesh_tensor[random_sample_idx]
+    
+    def _update_vessel_fluid_point_tensor(self, vessel_idx):
+        """
+        Updates the current vessel tensor based on the given vessel index.
+
+        Parameters:
+        - vessel_idx (int): The index of the vessel.
+
+        Returns:
+        - torch.Tensor: The tensor representing the vessel's mesh data.
+        """
+        vessel = self.vessel_files[vessel_idx]
+        fluid_data = meshio.read(vessel['fluid'])
+        fluid_points = torch.Tensor(fluid_data.points)
+        return fluid_points
+    
+    def _update_vessel_sys_vel_tensor(self, vessel_idx):
+        """
+        Updates the current vessel tensor based on the given vessel index.
+
+        Parameters:
+        - vessel_idx (int): The index of the vessel.
+
+        Returns:
+        - torch.Tensor: The tensor representing the vessel's mesh data.
+        """
+        vessel = self.vessel_files[vessel_idx]
+        fluid_data = meshio.read(vessel['fluid'])
+        sys_vel = torch.Tensor(fluid_data.point_data['velocity_systolic'])
+        return sys_vel
+    
+    def _get_vessel_files(self):
+        """
+        Retrieves the vessel files from the given directory.
+
+        Returns:
+        - list: A list of dictionaries, where each dictionary contains the paths to the fluid and mesh files.
+        """
+        vessel_files = []
+        for fluid_filename in os.listdir(self.data_path):
+            if fluid_filename.endswith('fluid.vtu'):
+                fluid_file_path = os.path.join(self.data_path, fluid_filename)
+                mesh_file_path = fluid_file_path.replace('fluid.vtu', 'wss.vtu')
+                vessel_files.append({
+                    'fluid': fluid_file_path,
+                    'mesh': mesh_file_path
+                })
+        return vessel_files
+        
