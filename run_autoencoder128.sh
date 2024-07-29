f7f37061ca6d49f6606e3d95afb458e42bf73687
Eric Kaufmann <eric.kaufmann291@gmail.com>
2024-07-29 14:00:56 +0200

    refactor: Update run_autoencoder128.sh to use a higher learning rate and add metadata flag

--- run_autoencoder128.sh
+++ run_autoencoder128.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=./output/autoencoder128_%j.out
 #SBATCH --error=./output/autoencoder128_%j.err
 #SBATCH --ntasks=1
-#SBATCH --partition=vis
+#SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --mem=16G
 
@@ -11,4 +11,4 @@ PROJECT_DIR=$HOME/workspace/vascunet
 
 source $PROJECT_DIR/.venv/bin/activate
 
-python $PROJECT_DIR/src/train_autoencoder128.py --learning_rate 0.00001 --job_id $SLURM_JOB_ID --num_epochs 300 --batch_size 1 --vector_input
+python $PROJECT_DIR/src/train_autoencoder128.py --learning_rate 0.001 --job_id $SLURM_JOB_ID --num_epochs 300 --batch_size 1 --vector_input --add_metadata
