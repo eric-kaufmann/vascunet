2f11692735f21a89afc8d045a7095996bf8944f3
Eric Kaufmann <eric.kaufmann291@gmail.com>
2024-07-30 16:33:05 +0200

    refactor: Update run_autoencoder128.sh to use a lower learning rate and add log_every parameter

--- run_autoencoder128.sh
+++ run_autoencoder128.sh
@@ -3,7 +3,8 @@
 #SBATCH --output=./output/autoencoder128_%j.out
 #SBATCH --error=./output/autoencoder128_%j.err
 #SBATCH --ntasks=1
-#SBATCH --partition=gpu
+#SBATCH --time=10:00:00
+#SBATCH --partition=vascunet
 #SBATCH --gres=gpu:1
 #SBATCH --mem=16G
 
@@ -11,4 +12,4 @@ PROJECT_DIR=$HOME/workspace/vascunet
 
 source $PROJECT_DIR/.venv/bin/activate
 
-python $PROJECT_DIR/src/train_autoencoder128.py --learning_rate 0.001 --job_id $SLURM_JOB_ID --num_epochs 300 --batch_size 1 --vector_input --add_metadata
+python $PROJECT_DIR/src/train_autoencoder128.py --learning_rate 0.00001 --job_id $SLURM_JOB_ID --num_epochs 300 --batch_size 1 --log_every 400 --vector_input --add_metadata
